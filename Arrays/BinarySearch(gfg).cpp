96c1ad701ff4e7c1aff2cecb8106e23ddb36f3a1
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2022-12-28 12:38:00 +0530

    Update BinarySearch(gfg).cpp

--- Arrays/BinarySearch(gfg).cpp
+++ Arrays/BinarySearch(gfg).cpp
@@ -1,3 +1,10 @@
+/*Step-by-step Binary Search Algorithm: We basically ignore half of the elements just after one comparison.
+
+Compare x with the middle element.
+If x matches with the middle element, we return the mid index.
+Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element. So we recur for the right half.
+Else (x is smaller) recur for the left half.*/
+
 class Solution {
   public:
   int helper(int arr[],int k,int s,int e){
