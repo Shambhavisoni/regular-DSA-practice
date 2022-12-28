d07466c6ad6731f7c2573e349aaac13b9cf89f95
Shambhavisoni <104761406+Shambhavisoni@users.noreply.github.com>
2022-12-28 13:35:32 +0530

    Update BinarySearch(gfg).cpp

--- Arrays/BinarySearch(gfg).cpp
+++ Arrays/BinarySearch(gfg).cpp
@@ -3,7 +3,13 @@
 Compare x with the middle element.
 If x matches with the middle element, we return the mid index.
 Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element. So we recur for the right half.
-Else (x is smaller) recur for the left half.*/
+Else (x is smaller) recur for the left half.
+
+****Algo used:decrease and conquer*****
+
+Here we are using 
+int mid = low + (high - low)/2;
+Because, it fails if the sum of low and high is greater than the maximum positive int value(2^31 - 1 ).*/
 
 class Solution {
   public:
@@ -42,6 +48,25 @@ class Solution {
               return helper(arr,k,mid+1,e);
           }
       }
+    /*Time Complexity: O(log n)
+    Auxiliary Space: O(log n)*/
+    
+    //approach 3
+    while(s<=e){
+          int mid=s+(e-s)/2;
+          if(arr[mid]==k){
+              return mid;
+          }
+          else if(arr[mid]<k){
+              s=mid+1;
+          }
+          else if(arr[mid]>k){
+              e=mid-1;
+          }
+      }
+      return -1;
+    /*Time Complexity: O(log n)
+    Auxiliary Space: O(1)*/
   }
     int binarysearch(int arr[], int n, int k) {
         // code here
